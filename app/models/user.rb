5e80a0a2f0b7cc1f7b3d9a3104fb8d06f22663f8
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-07-05 14:04:34 +0100

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
     has_secure_password
     has_many :favourite_countries, :class_name => "Country"
-
+    has_many :visited_countries, class_name => "Country"
+    has_many :bucket_list_countries, class_name => "Country"
 
 end
--- db/migrate/20190703133437_create_users.rb
+++ db/migrate/20190703133437_create_users.rb
@@ -3,9 +3,6 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :password 
-      t.interger :favourite_countries 
-      t.interger :visited_countries
-      t.interger :bucket_list
     end
   end
 end
