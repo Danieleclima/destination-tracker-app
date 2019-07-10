111929ef73ce58c05e8db0f9036baa3193e39553
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-07-10 20:03:37 +0100

    dropdown country id

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -4,5 +4,10 @@ class TripController < ApplicationController
     get '/trips/new' do 
         erb :'/trip/new' 
     end 
+
+    post '/trips' do
+       @trip =  Trip.create(params[:trip])
+       binding.pry
+    end
     
 end
